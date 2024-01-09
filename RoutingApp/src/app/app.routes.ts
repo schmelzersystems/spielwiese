9af80ad1804c43d7a237b0cfbd8ee86c2e4a7a5f
Ruben R. Schmelzer <email@schmelzersystems.com>
2024-01-09 10:52:16 +0100

    Routes definiert

--- RoutingApp/src/app/app.routes.ts
+++ RoutingApp/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {Comp1Component} from "../components/comp1/comp1.component";
+import {Comp2Component} from "../components/comp2/comp2.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path:'/comp1', component: Comp1Component},
+  { path:'/comp2', component: Comp2Component}
+];
